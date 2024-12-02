261b9a4846507768c0e40bdce6816c59da59e18d
Mzoughi Mohamed Khalil <121819160+EIMzoughi@users.noreply.github.com>
2024-12-02 16:12:26 +0100

    fixing ball movement bug
    calculate angle
    updates conditions
    reset game logic

--- Pong/Main.cpp
+++ Pong/Main.cpp
@@ -1,5 +1,6 @@
 #include <SDL.h>
 #include <stdio.h>
+#include <algorithm>
 
 const int WINDOW_SCREEN_WIDTH = 640;
 const int WINDOW_SCREEN_HEIGHT = 480;
@@ -68,39 +69,96 @@ void MovePlayer(SDL_Rect* rect, int speed)
     rect->y += speed; 
 }
 
-void MoveBall(SDL_Rect* rect, Speed& speed)
+void ResetGame()
 {
-    if (ball.x == player1.x + player1.w)
+    ball.x = (WINDOW_SCREEN_WIDTH - ball.w) / 2;
+    ball.y = (WINDOW_SCREEN_HEIGHT - ball.h) / 2;
+    player1.y = player2.y = (WINDOW_SCREEN_HEIGHT - playerHight) / 2;
+        
+}
+
+void MoveBall(SDL_Rect* ball, Speed& speed)
+{
+   
+
+    // wall collision 
+    if (ball->y <= 0 || ball->y + ball->h >= WINDOW_SCREEN_HEIGHT)
     {
-        if ((ball.y <= player1.y + player1.h) && (ball.y + ball.h >= player1.y))
+        ballSpeed.y = -ballSpeed.y; // Reverse vertical direction
+        ball->y = std::max(0, std::min(ball->y, WINDOW_SCREEN_HEIGHT - ball->h)); // Clamp position
+    }
+
+    // Paddle 1 collision (left side)
+    if (ball->x <= player1.x + player1.w && ball->x > player1.x)
+    {
+        if (ball->y + ball->h > player1.y && ball->y < player1.y + player1.h)
         {
-            speed.x = -speed.x;
-            double hitfraction = double((ball.y + ball.h) / 2 - (player1.y + playerHight) / 2) / (playerHight);
-            //speed.x = fabs(1-hitfraction) * speed.x;
-            speed.y = hitfraction > 0 ? hitfraction * (double)initBallSpeed : -hitfraction * (double)initBallSpeed;
-            printf("%f",(float)speed.y);
-        }
+            // Reverse x direction
+            ballSpeed.x = -ballSpeed.x;
 
-    }
-    if (ball.x + ball.w == player2.x)
-    {
-        if ((ball.y <= player2.y + player2.h) && (ball.y + ball.h >= player2.y))
+            // reflecton angle
+            double hitPoint = (ball->y + ball->h / 2.0) - (player1.y + player1.h / 2.0);
+            double normalizedHitPoint = hitPoint / (player1.h / 2.0);
+            
+            // max bounce angle is 75 degree
+            double maxAngle = 75.0 * M_PI / 180.0; // Convert to radians
+
+            // Calculate new speed components
+            double bounceAngle = normalizedHitPoint * maxAngle;
+            double ballSpeedMagnitude = sqrt(ballSpeed.x * ballSpeed.x + ballSpeed.y * ballSpeed.y);
+
+           
+            ballSpeed.y = sin(bounceAngle) * ballSpeedMagnitude;
+
+            // Ensure ball doesn't get stuck in the paddle
+            ball->x = player1.x + player1.w + 1;
+        }
+        else
         {
-            speed.x = -speed.x;
-            double hitfraction = double((((ball.y + ball.h) / 2 - (player2.y + playerHight) /2)) / (playerHight));
-            //speed.x = fabs(1-hitfraction) * speed.x;
-            speed.y = hitfraction > 0 ? hitfraction * (double)initBallSpeed : -hitfraction * (double)initBallSpeed;
-            printf("%f", (float)speed.y);
+            //reset game and add score
+            ResetGame();
+            ballSpeed.x = -initBallSpeed;
+            ballSpeed.y = 0; // Reset vertical speed
         }
     }
 
-    if (ball.y <= 0 || ball.y >= WINDOW_SCREEN_HEIGHT - 20)
+    // Paddle 2 collision (right side)
+    if (ball->x + ball->w >= player2.x && ball->x + ball->w < player2.x + player2.w)
     {
-        speed.y = -speed.y;
+        if (ball->y + ball->h > player2.y && ball->y < player2.y + player2.h)
+        {
+            // Reverse horizontal direction
+            ballSpeed.x = -ballSpeed.x;
+
+            // reflecton angle
+            double hitPoint = (ball->y + ball->h / 2.0) - (player2.y + player2.h / 2.0);
+            double normalizedHitPoint = hitPoint / (player2.h / 2.0);
+
+            // max bounce angle is 75 degree
+            double maxAngle = 75.0 * M_PI / 180.0; // Convert to radians
+
+            // Calculate new speed components
+            double bounceAngle = normalizedHitPoint * maxAngle;
+            double ballSpeedMagnitude = sqrt(ballSpeed.x * ballSpeed.x + ballSpeed.y * ballSpeed.y);
+
+            
+            ballSpeed.y = sin(bounceAngle) * ballSpeedMagnitude;
+
+            // Ensure ball doesn't get stuck in the paddle
+            ball->x = player2.x - ball->w - 1;
+        }
+        else
+        {
+            // reset game and add score
+            ResetGame();
+            ballSpeed.x = initBallSpeed;
+            ballSpeed.y = 0; // Reset vertical speed
+        }
     }
 
-    ball.x += speed.x;
-    ball.y += speed.y;
+    // Update ball position
+    ball->x += ballSpeed.x;
+    ball->y += ballSpeed.y;
 }
 
 int main(int argc, char* argv[])
@@ -114,12 +172,11 @@ int main(int argc, char* argv[])
 
     SDL_Rect Border = { WINDOW_SCREEN_WIDTH/2 ,0,1,WINDOW_SCREEN_HEIGHT };
     SDL_RenderFillRect(renderer, &Border);
-
-    
-
+   
     SDL_Event event;
     int running = 1;
     int movingPlayer1 = 0, movingPlayer2 = 0;//act like bool and direction for player //0 player is not moving //1 moving down //-1 moving up
+
     while (running)
     {
         if (SDL_PollEvent(&event))
