ff4108b410be0349bfe852a31164b96fb609a2da
Mzoughi Mohamed Khalil <121819160+EIMzoughi@users.noreply.github.com>
2024-12-02 22:22:00 +0100

    sdl_ttf setup / display score

--- Pong/Main.cpp
+++ Pong/Main.cpp
@@ -1,18 +1,21 @@
 #include <SDL.h>
+#include <SDL_ttf.h>
 #include <stdio.h>
 #include <algorithm>
+#include <string>
 
 const int WINDOW_SCREEN_WIDTH = 640;
 const int WINDOW_SCREEN_HEIGHT = 480;
 
 static int movementSpeed = 10;//player speed
-static int initBallSpeed = 10;
-static int playerWidth = 40;
-static int playerHight = 200;
+static int initBallSpeed = 5;
+static int playerWidth = 20;
+static int playerHight = 100;
 
 
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
+TTF_Font* font = NULL;
 
 SDL_Rect player1 = { 20, (WINDOW_SCREEN_HEIGHT - playerHight) / 2, playerWidth, playerHight };
 SDL_Rect player2 = { WINDOW_SCREEN_WIDTH - 60, (WINDOW_SCREEN_HEIGHT - playerHight) / 2, playerWidth, playerHight };
@@ -23,8 +26,15 @@ typedef struct speed
     int x;
     int y;
 }Speed;
-
 Speed ballSpeed = { initBallSpeed,0 };//init ball speed
+
+typedef struct score
+{
+    int player1;
+    int player2;
+} Score;
+Score score = {0,0};
+
 
 int Initisialize()
 {
@@ -46,7 +56,45 @@ int Initisialize()
         printf("renderer could not be created SDLerror:%s", SDL_GetError());
         return 0;
     }
+    if (TTF_Init() == -1)
+    {
+        printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+        return 0;
+    }
+    font = TTF_OpenFont("Debrosee-ALPnL.ttf", 50);
+    if (font == NULL)
+    {
+        printf("Failed to load font! SDL_ttf Error: %s\n", TTF_GetError());
+        return 0;
+    }
+
     return 1;
+}
+// Function to render text as a texture
+void RenderText(const std::string& text, int x, int y)
+{
+    SDL_Color white = { 255, 255, 255, 255 }; // Text color
+    SDL_Surface* textSurface = TTF_RenderText_Solid(font, text.c_str(), white);
+    if (textSurface == NULL)
+    {
+        printf("Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError());
+        return;
+    }
+
+    SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+    if (textTexture == NULL)
+    {
+        printf("Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError());
+    }
+    else
+    {
+        SDL_Rect renderQuad = { x, y, textSurface->w, textSurface->h };
+        SDL_RenderCopy(renderer, textTexture, NULL, &renderQuad);
+    }
+
+    // Clean up
+    SDL_FreeSurface(textSurface);
+    SDL_DestroyTexture(textTexture);
 }
 
 void ClearScreen()
@@ -58,10 +106,14 @@ void ClearScreen()
 void UpdateScreen()
 {
     ClearScreen();
+
     SDL_SetRenderDrawColor(renderer, 225, 255, 255, 255);
     SDL_RenderFillRect(renderer, &player1);
     SDL_RenderFillRect(renderer, &player2);
     SDL_RenderFillRect(renderer, &ball);
+
+    RenderText(std::to_string(score.player1), 100, 100);
+
     SDL_RenderPresent(renderer);
 }
 void MovePlayer(SDL_Rect* rect, int speed)
@@ -77,6 +129,7 @@ void ResetGame()
         
 }
 
+
 void MoveBall(SDL_Rect* ball, Speed& speed)
 {
    
@@ -91,7 +144,7 @@ void MoveBall(SDL_Rect* ball, Speed& speed)
     // Paddle 1 collision (left side)
     if (ball->x <= player1.x + player1.w && ball->x > player1.x)
     {
-        if (ball->y + ball->h > player1.y && ball->y < player1.y + player1.h)
+        if (ball->y + ball->h >= player1.y && ball->y <= player1.y + player1.h)
         {
             // Reverse x direction
             ballSpeed.x = -ballSpeed.x;
@@ -116,6 +169,7 @@ void MoveBall(SDL_Rect* ball, Speed& speed)
         else
         {
             //reset game and add score
+            score.player2++;
             ResetGame();
             ballSpeed.x = -initBallSpeed;
             ballSpeed.y = 0; // Reset vertical speed
@@ -125,7 +179,7 @@ void MoveBall(SDL_Rect* ball, Speed& speed)
     // Paddle 2 collision (right side)
     if (ball->x + ball->w >= player2.x && ball->x + ball->w < player2.x + player2.w)
     {
-        if (ball->y + ball->h > player2.y && ball->y < player2.y + player2.h)
+        if (ball->y + ball->h >= player2.y && ball->y <= player2.y + player2.h)
         {
             // Reverse horizontal direction
             ballSpeed.x = -ballSpeed.x;
@@ -150,6 +204,7 @@ void MoveBall(SDL_Rect* ball, Speed& speed)
         else
         {
             // reset game and add score
+            score.player1++;
             ResetGame();
             ballSpeed.x = initBallSpeed;
             ballSpeed.y = 0; // Reset vertical speed
@@ -159,6 +214,26 @@ void MoveBall(SDL_Rect* ball, Speed& speed)
     // Update ball position
     ball->x += ballSpeed.x;
     ball->y += ballSpeed.y;
+}
+void HandleInput()
+{
+    //handle players movement:
+        // Get current keyboard state
+    const Uint8* keystate = SDL_GetKeyboardState(NULL);
+
+    // Player 1 Movement
+    int movingPlayer1 = 0;
+    if (keystate[SDL_SCANCODE_W] && player1.y > 0) movingPlayer1 = -1;
+    if (keystate[SDL_SCANCODE_S] && player1.y < WINDOW_SCREEN_HEIGHT - player1.h) movingPlayer1 = 1;
+
+    // Player 2 Movement
+    int movingPlayer2 = 0;
+    if (keystate[SDL_SCANCODE_UP] && player2.y > 0) movingPlayer2 = -1;
+    if (keystate[SDL_SCANCODE_DOWN] && player2.y < WINDOW_SCREEN_HEIGHT - player2.h) movingPlayer2 = 1;
+
+    //move players
+    if (movingPlayer1) MovePlayer(&player1, movingPlayer1 * movementSpeed);
+    if (movingPlayer2) MovePlayer(&player2, movingPlayer2 * movementSpeed);
 }
 
 int main(int argc, char* argv[])
@@ -175,7 +250,6 @@ int main(int argc, char* argv[])
    
     SDL_Event event;
     int running = 1;
-    int movingPlayer1 = 0, movingPlayer2 = 0;//act like bool and direction for player //0 player is not moving //1 moving down //-1 moving up
 
     while (running)
     {
@@ -190,23 +264,8 @@ int main(int argc, char* argv[])
         //ball movement
         MoveBall(&ball, ballSpeed);
                        
-        //handle players movement:
-        // Get current keyboard state
-        const Uint8* keystate = SDL_GetKeyboardState(NULL);
+        HandleInput();
 
-        // Player 1 Movement
-        int movingPlayer1 = 0;
-        if (keystate[SDL_SCANCODE_W] && player1.y > 0) movingPlayer1 = -1;
-        if (keystate[SDL_SCANCODE_S] && player1.y < WINDOW_SCREEN_HEIGHT - player1.h) movingPlayer1 = 1;
-
-        // Player 2 Movement
-        int movingPlayer2 = 0;
-        if (keystate[SDL_SCANCODE_UP] && player2.y > 0) movingPlayer2 = -1;
-        if (keystate[SDL_SCANCODE_DOWN] && player2.y < WINDOW_SCREEN_HEIGHT - player2.h) movingPlayer2 = 1;
-
-        //move players
-        if (movingPlayer1) MovePlayer(&player1, movingPlayer1 * movementSpeed);
-        if (movingPlayer2) MovePlayer(&player2, movingPlayer2 * movementSpeed);
         UpdateScreen();
 
         //+-60fps
